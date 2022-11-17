fcc26d6e72202b8efb28baecbcd3aa4be7664aa1
Melissa Hiatt <melissahiatt@gmail.com>
2022-11-17 15:05:21 -0800

    cleaned up icons

--- blocks/build/contact-group/index.asset.php
+++ blocks/build/contact-group/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '42accc909ce8a8cfb634');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'd3c20985d265fc821d92');
--- blocks/build/contact-info/index.asset.php
+++ blocks/build/contact-info/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '16dff98336694aebfad9');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '77a169a39b356a625584');
