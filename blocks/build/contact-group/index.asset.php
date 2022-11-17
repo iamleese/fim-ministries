87ad0e1cad34601c264c148952cdf30ed845c73e
Melissa Hiatt <melissahiatt@gmail.com>
2022-11-17 15:09:53 -0800

    build 11.17.2022

--- blocks/build/contact-group/index.asset.php
+++ blocks/build/contact-group/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'd3c20985d265fc821d92');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'b3804efe11f54d0f60ef');
