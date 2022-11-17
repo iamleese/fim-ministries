aa579583063aad99abc40f8248565e3bc1b40871
Melissa Hiatt <melissahiatt@gmail.com>
2022-11-17 15:08:36 -0800

    fixed spelling error of icon variable

--- blocks/build/ministry-category-description/index.asset.php
+++ blocks/build/ministry-category-description/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '8878077d61624ce65441');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '75d6a05450a0c5538661');
