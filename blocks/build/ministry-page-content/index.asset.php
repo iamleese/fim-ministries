a37c4f0519870d80fc12e38a60dbf1755f018c9c
Melissa Hiatt <melissahiatt@gmail.com>
2022-11-18 12:24:40 -0800

    build 11/18/2022

--- blocks/build/ministry-page-content/index.asset.php
+++ blocks/build/ministry-page-content/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '668c164cc1a6b88d46f9');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '03a1f26816c30da5943f');
